162f5ce98561993cec3d8884ef2f40ab2670bfc1
Gourav-G-Revankar <gourav.revankar35@gmail.com>
2022-03-14 14:52:41 +0530

    lifing state up component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,30 @@
+import React, { Component } from 'react'
 import './App.css';
 import Menu from './Components/MenuComponent';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className='container'>
-          <NavbarBrand>Navbar</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render(){
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className='container'>
+            <NavbarBrand>Navbar</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
